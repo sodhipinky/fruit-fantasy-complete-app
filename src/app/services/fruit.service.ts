b09c72d2c95eacd28cfe7247f79c1b9b9f170b88
Pinky Sodhi <pinky.sodhi@niit.com>
2023-06-25 11:52:22 +0530

    defined url attribute

--- src/app/services/fruit.service.ts
+++ src/app/services/fruit.service.ts
@@ -1,10 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FruitService {
 
+  url = 'http://localhost:3000/fruits';
   constructor(private httpClient : HttpClient) { }
 }
