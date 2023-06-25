5970fd2b2a6d248495d7011a9a19903e61d7072f
Pinky Sodhi <pinky.sodhi@niit.com>
2023-06-25 12:31:11 +0530

    added fruit list component to fruit manager

--- src/app/fruit-list/fruit-list.component.ts
+++ src/app/fruit-list/fruit-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Fruit } from '../models/fruit';
 
 @Component({
@@ -7,5 +7,7 @@ import { Fruit } from '../models/fruit';
   styleUrls: ['./fruit-list.component.css']
 })
 export class FruitListComponent {
+
+  @Input()
   fruitList : Fruit[] = []
 }
