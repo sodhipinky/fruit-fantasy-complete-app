2764d68aee6d0baeed5181b5be837975e8165550
Pinky Sodhi <pinky.sodhi@niit.com>
2023-06-25 11:37:58 +0530

    created header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavPanelComponent
+    NavPanelComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
