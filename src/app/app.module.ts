89c3ac2e95eff9f9869ecbf7d32febe5195f6b2a
Pinky Sodhi <pinky.sodhi@niit.com>
2023-06-25 10:33:23 +0530

    removed nav bar module from app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavPanelComponent } from './nav-panel/nav-panel.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -15,7 +14,6 @@ import { MatListModule } from '@angular/material/list';
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
     NavPanelComponent
   ],
   imports: [
