e9a90dce823f8a383ae595602bb8962fc1f77a93
Pinky Sodhi <pinky.sodhi@niit.com>
2023-06-26 14:15:21 +0530

    defined fruit property defined onint for getting fruit by id

--- src/app/fruit-detail/fruit-detail.component.ts
+++ src/app/fruit-detail/fruit-detail.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Fruit } from '../models/fruit';
+import { FruitService } from '../services/fruit.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-fruit-detail',
   templateUrl: './fruit-detail.component.html',
   styleUrls: ['./fruit-detail.component.css']
 })
-export class FruitDetailComponent {
+export class FruitDetailComponent implements OnInit {
+  fruit: Fruit = {}
 
+  constructor(
+    private fruitService: FruitService,
+    private activatedRoute: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((params) => {
+      let id = params.get('id') ?? 0
+      this.fruitService.getFruitById(+id).subscribe((fruit) => {
+        this.fruit = fruit
+      })
+    })
+  }
 }
