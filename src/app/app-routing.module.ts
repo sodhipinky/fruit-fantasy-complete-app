dde05de9cd1b6ce3417fb85d8d9cae0d3e7563ea
Pinky Sodhi <pinky.sodhi@niit.com>
2023-06-26 14:34:08 +0530

    defined route for fruit detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FruitManagerComponent } from './fruit-manager/fruit-manager.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { FruitDetailComponent } from './fruit-detail/fruit-detail.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,10 @@ const routes: Routes = [
     path: '',
     redirectTo: '/home',
     pathMatch: 'full'
+  },
+  {
+    path: 'fruit-detail/:id',
+    component: FruitDetailComponent
   },
   {
     path: '**',
