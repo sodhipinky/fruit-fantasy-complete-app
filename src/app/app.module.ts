898daaf4414fc47d3d75909831b42887ef4ff079
Pinky Sodhi <pinky.sodhi@niit.com>
2023-06-26 12:48:53 +0530

    created page-not-found component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule} from '@angular/material/input';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { MatExpansionModule } from '@angular/material/expansion';
     FruitListComponent,
     SearchComponent,
     AddFruitComponent,
-    FruitCardComponent
+    FruitCardComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
