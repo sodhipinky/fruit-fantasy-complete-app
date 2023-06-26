ab9adb430f41e606a8482841d0054aec68f806b5
Pinky Sodhi <pinky.sodhi@niit.com>
2023-06-26 12:39:05 +0530

    defined route for home as fruit manager

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FruitManagerComponent } from './fruit-manager/fruit-manager.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: FruitManagerComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
