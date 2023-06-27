6886a7a2a39bbdb053ff0cff2dc3317202ae5b6a
Pinky Sodhi <pinky.sodhi@niit.com>
2023-06-27 10:32:23 +0530

    defined route for login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { FruitManagerComponent } from './fruit-manager/fruit-manager.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { FruitDetailComponent } from './fruit-detail/fruit-detail.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
@@ -10,13 +11,17 @@ const routes: Routes = [
     component: FruitManagerComponent
   },
   {
+    path: 'fruit-detail/:id',
+    component: FruitDetailComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
     path: '',
     redirectTo: '/home',
     pathMatch: 'full'
-  },
-  {
-    path: 'fruit-detail/:id',
-    component: FruitDetailComponent
   },
   {
     path: '**',
