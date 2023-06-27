0fc282bf1e99e70e1581b74e81bfae57fa30e224
Pinky Sodhi <pinky.sodhi@niit.com>
2023-06-27 10:34:48 +0530

    injected authentication and router services

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from '../services/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  constructor(
+    private authenticationService: AuthenticationService,
+    private router: Router) { }
 }
