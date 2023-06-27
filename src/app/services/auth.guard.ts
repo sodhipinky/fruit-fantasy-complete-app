b6a9bc695bc3d32c4387ce8e184399e3316ed854
Pinky Sodhi <pinky.sodhi@niit.com>
2023-06-27 11:56:00 +0530

    defined canActivate guard

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -1,5 +1,16 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthenticationService } from './authentication.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const authenticationService = inject(AuthenticationService)
+  const router = inject(Router)
+
+  if(authenticationService.isLoggedIn){
+    return true;
+  }
+  else {
+    router.navigate(['/login'])
+    return false;
+  }
 };
