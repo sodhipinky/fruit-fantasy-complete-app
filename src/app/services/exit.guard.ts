6e6a2b32ba0d96c54a095f072f8d77c7fce42e2b
Pinky Sodhi <pinky.sodhi@niit.com>
2023-06-27 13:35:57 +0530

    defined exit guard for FruitDetail

--- src/app/services/exit.guard.ts
+++ src/app/services/exit.guard.ts
@@ -1,5 +1,6 @@
 import { CanDeactivateFn } from '@angular/router';
+import { FruitDetailComponent } from '../fruit-detail/fruit-detail.component';
 
-export const exitGuard: CanDeactivateFn<unknown> = (component, currentRoute, currentState, nextState) => {
-  return true;
+export const exitGuard: CanDeactivateFn<FruitDetailComponent> = (fruitDetailComponent, currentRoute, currentState, nextState) => {
+  return fruitDetailComponent.isEditComplete();
 };
