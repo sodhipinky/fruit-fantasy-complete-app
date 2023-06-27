c3d07fc85ab962407943515744c66af790448d49
Pinky Sodhi <pinky.sodhi@niit.com>
2023-06-27 10:28:55 +0530

    defined login and logout methods

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -4,8 +4,16 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthenticationService {
-  
+
   isLoggedIn: boolean = false
 
   constructor() { }
+
+  login() {
+    this.isLoggedIn = true
+  }
+
+  logout() {
+    this.isLoggedIn = false
+  }
 }
