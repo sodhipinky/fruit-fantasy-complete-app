a3ba114326386ede95dbc641028fb4e332cb5321
Pinky Sodhi <pinky.sodhi@niit.com>
2023-06-27 12:03:39 +0530

    added the guard to home and fruit-detail route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,18 @@ import { FruitManagerComponent } from './fruit-manager/fruit-manager.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { FruitDetailComponent } from './fruit-detail/fruit-detail.component';
 import { LoginComponent } from './login/login.component';
+import { authGuard } from './services/auth.guard';
 
 const routes: Routes = [
   {
     path: 'home',
-    component: FruitManagerComponent
+    component: FruitManagerComponent,
+    canActivate: [authGuard]
   },
   {
     path: 'fruit-detail/:id',
-    component: FruitDetailComponent
+    component: FruitDetailComponent,
+    canActivate: [authGuard]
   },
   {
     path: 'login',
